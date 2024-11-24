bbedfe49e232a0d908d61c93f2c801253561186b
bharath reddy <bharathreddy@Bharath-Reddy.attlocal.net>
2024-11-24 13:06:05 -0600

    local

--- amazon-rp/src/App.js
+++ amazon-rp/src/App.js
@@ -1,24 +1,11 @@
-import "./App.css";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Checkout from "./components/Checkout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { StateProvider } from "./components/StateProvider"; // Import StateProvider
-import  { initialState } from "./components/Reducer"; // Import reducer and initialState
-import reducer from "./components/Reducer";
+import React from 'react'
 
 function App() {
   return (
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </Router>
-    </StateProvider>
-  );
+    <div>
+      hehehhe 
+    </div>
+  )
 }
 
-export default App;
+export default App
