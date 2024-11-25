a4ece73f317ad8cc77c732533c9fa7af2cf76e97
bharath reddy <bharathreddy@Bharath-Reddy.attlocal.net>
2024-11-24 21:38:52 -0600

    api start changes

---
+++ amazon-rp/src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const searchImages = async (term) => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
+    headers: {
+      Authorization: 'Client-ID WqtAJwDFa-G6J54dxazu9fnBbpgJLKYC84OIU5dDnEg',
+    },
+    params: {
+      query: term,
+    },
+  });
+
+
+  return response.results.data;
+};
+
+export default searchImages;
+
+
