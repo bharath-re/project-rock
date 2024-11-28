a2ef42ae62150ba091aecb9ac18ae4bd1763988f
bharath reddy <bharathreddy@Bharath-Reddy.attlocal.net>
2024-11-28 17:55:44 -0600

    final changes after editing

--- amazon-rp/src/components/BookShow.js
+++ amazon-rp/src/components/BookShow.js
@@ -1,41 +1,48 @@
-import { useState } from 'react';
-import '../App.css';
-import BookEdit from './BookEdit';
+import { useState } from "react";
+import "../App.css";
+import BookEdit from "./BookEdit";
 
-function BookShow({book, onDelete, onEdit }) {
-    const [showEdit,setShowEdit]=useState(false);
+function BookShow({ book, onDelete, onEdit }) {
+  const [showEdit, setShowEdit] = useState(false);
 
-    const handleClickDelete = () => {
-        onDelete(book.id);
-    };
+  const handleClickDelete = () => {
+    onDelete(book.id);
+  };
 
-   const handleClickEdit = () => {
+  const handleClickEdit = () => {
     setShowEdit(!showEdit);
-   }
+  };
 
-   const handleSubmit = (id,title) => {
+  const handleSubmit = (id, title) => {
     setShowEdit(false);
-    onEdit(id,title);
-   }
+    onEdit(id, title);
+  };
 
-    let content = <h3>{book.title}</h3>
-    if(showEdit) {
-        content =<BookEdit book={book} onSubmit={handleSubmit}/> 
-    }
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit book={book} onSubmit={handleSubmit} />;
+  }
 
-   
   return (
-    <div className='book-show'>
-        <div>{content}</div>
+    <div className="book-show">
+      <div>{content}</div>
+      <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/200/300`}
+      ></img>
 
-      <div className='actions'>
-        <button className='delete' onClick={handleClickDelete}> Delete </button>
-        <button className='edit' onClick={handleClickEdit}> edit </button>
-
-
+      <div className="actions">
+        <button className="delete" onClick={handleClickDelete}>
+          {" "}
+          Delete{" "}
+        </button>
+        <button className="edit-1" onClick={handleClickEdit}>
+          {" "}
+          edit{" "}
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default BookShow
+export default BookShow;
